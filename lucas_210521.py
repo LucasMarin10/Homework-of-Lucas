70d81edd6cb8adc0f718ccee40da65fc69208141
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-05-21 22:13:30 -0300

    Add files via upload

---
+++ lucas_210521.py
@@ -0,0 +1,42 @@
+#question 2
+name="Lucas"
+#question 3
+age=10
+#question 4
+print(name+str(age))
+#question 5
+print("Lucas\nMarin")
+#question 6
+print("Lucas\n\tMarin")
+#question 7
+bestgc="   Snake   "
+#question 8
+print(bestgc)
+#question 9
+print(bestgc+"!")
+#question 10
+print(bestgc.strip()+"!")
+#question 11
+print(bestgc.lstrip()+"!")
+#question 12
+fruits=["banana","apple","orange"]
+#question 13
+print(fruits[1])
+#question 14
+print(fruits[-3])
+#question 15
+fruits.append("brocoli")
+#question 16
+print(fruits)
+#question 17
+fruits[0]="toyota"
+#question 18
+print(fruits)
+#question 19
+random=fruits[0]+" "+fruits[1]
+#question 20
+print(random)
+#question 21
+fruitR=fruits[3].upper
+#question 22
+print(fruitR)
